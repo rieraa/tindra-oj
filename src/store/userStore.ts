ee16fd4616e0cb9af53573f133a2dc780b148541
rieraa <ameee9@163.com>
2024-02-21 23:02:13 +0800

    ✨ 全局存储用户信息

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { UserControllerService } from "@/api/services/UserControllerService";
 
 // 你可以对 `defineStore()` 的返回值进行任意命名，但最好使用 store 的名字，同时以 `use` 开头且以 `Store` 结尾。(比如 `useUserStore`，`useCartStore`，`useProductStore`)
 // 第一个参数是你的应用中 Store 的唯一 ID。
@@ -6,25 +7,25 @@ export const useUserStore = defineStore("user", {
   state: () => {
     return {
       userInfo: {
-        avatar:
-          "https://oooooo.oss-cn-fuzhou.aliyuncs.com/readme/202312232252586.jpg",
-        name: "user",
         isAdmin: false,
-        loginStatus: false,
+        loginStatus: null as boolean | null,
       },
     };
   },
   getters: {},
   actions: {
-    // 模拟登陆操作
-    getUserLoginInfo() {
-      this.userInfo = {
-        loginStatus: true,
-        isAdmin: true,
-        avatar:
-          "https://oooooo.oss-cn-fuzhou.aliyuncs.com/readme/202312280036348.jpg",
-        name: "用户登录",
-      };
+    /**
+     * 获取用户登录信息
+     */
+    async getUserLoginInfo() {
+      const result = await UserControllerService.getLoginUser();
+      if (result.code === 0) {
+        this.userInfo = {
+          ...result.data,
+          loginStatus: true,
+          isAdmin: result.data === "admin",
+        };
+      }
     },
   },
 });
