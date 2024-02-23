0e2d77ecba81d5cbd49bda49de80c5153963cc67
rieraa <ameee9@163.com>
2024-02-23 23:07:07 +0800

    🐛 隐藏用户布局渲染在菜单列表

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,6 +11,9 @@ export const routes: Array<RouteRecordRaw> = [
     path: "/user",
     name: "用户",
     component: UserLayout,
+    meta: {
+      hide: true,
+    },
     children: [
       {
         path: "/user/login",
