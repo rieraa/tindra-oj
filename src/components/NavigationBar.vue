46a812307fe06f8b34a3d2fc4aa4a29eb868169b
rieraa <ameee9@163.com>
2024-02-25 00:20:44 +0800

    🐛 导航栏菜单渲染

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -15,15 +15,16 @@ const routeToUser = computed(() => {
   return routes.filter((item) => {
     // 没有设置 requireAuth 属性的菜单或者当前用户是管理员，则显示该菜单
     return (
-      (!item.meta?.requireAuth && !item.meta?.hide) ||
-      userStore.userInfo.isAdmin
+      (!item.meta?.requireAuth || userStore.userInfo.isAdmin) &&
+      !item.meta?.hide
     );
   });
 });
 
 // 被选中的菜单路由
 const selectedKeys = ref(["/"]);
-
+// 初始化选中的菜单
+selectedKeys.value = [route.path];
 // 监听路由变化 修改菜单栏样式
 watch(
   () => route.path,
@@ -44,45 +45,49 @@ const handleMenuItemClick = (key: string) => {
 </script>
 <!--全局导航栏组件-->
 <template>
-  <a-row id="navigationBar" class="grid-navigation" align="center">
-    <a-col flex="auto">
-      <a-menu
-        mode="horizontal"
-        @menu-item-click="handleMenuItemClick"
-        :selected-keys="selectedKeys"
-      >
-        <a-menu-item
-          key="0"
-          :style="{ padding: 0, marginRight: '38px' }"
-          disabled
+  <div id="navigationBar">
+    <a-row class="grid-navigation" align="center">
+      <a-col flex="auto">
+        <a-menu
+          mode="horizontal"
+          @menu-item-click="handleMenuItemClick"
+          :selected-keys="selectedKeys"
         >
-          <div class="logo">
-            <img class="img" src="../assets/logo.jpg" alt="logo" />
-          </div>
-        </a-menu-item>
-        <!--todo 过滤掉/user下的路由-->
-        <a-menu-item v-for="item in routeToUser" :key="item.path">
-          {{ item.name }}
-        </a-menu-item>
-      </a-menu>
-    </a-col>
-    <a-col flex="100px">
-      <a-avatar :image-url="userStore?.userInfo?.avatar ?? ''">A</a-avatar>
-    </a-col>
-  </a-row>
+          <a-menu-item
+            key="0"
+            :style="{ padding: 0, marginRight: '38px' }"
+            disabled
+          >
+            <div class="logo">
+              <img class="img" src="../assets/logo.jpg" alt="logo" />
+            </div>
+          </a-menu-item>
+          <!--todo 过滤掉/user下的路由-->
+          <a-menu-item v-for="item in routeToUser" :key="item.path">
+            {{ item.name }}
+          </a-menu-item>
+        </a-menu>
+      </a-col>
+      <a-col flex="100px">
+        <a-avatar :image-url="userStore?.userInfo?.userAvatar ?? ''"
+          >A</a-avatar
+        >
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-.grid-navigation {
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2);
+#navigationBar {
+  .grid-navigation {
+    .logo {
+      display: flex;
+      align-items: center;
 
-  .logo {
-    display: flex;
-    align-items: center;
-
-    .img {
-      width: 50px;
-      height: 50px;
+      .img {
+        width: 50px;
+        height: 50px;
+      }
     }
   }
 }
